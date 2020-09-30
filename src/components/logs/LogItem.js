3234ce1cc84d3d7903648e14ae387de41757a363
Abhishek <kumarabhishek0388@gmail.com>
2020-09-30 15:08:37 +0530

    fixing log item component

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import Moment from 'react-moment';
+
 import PropTypes from 'prop-types'
 
 const LogItem = ({log}) => {
@@ -6,6 +8,10 @@ const LogItem = ({log}) => {
         <li className = "collection-item">
             <div>
             <a href = "#edit-log-modal" className = {`modal-trigger ${log.attention ? 'red-text' : 'blue-text'}`}>{log.message}</a>
+            <br/>
+            <span className="grey-text">
+                <span className = "black-text">ID# {log.id}</span> Last updated by {' '}<span className = "black-text">{log.tech}</span>
+            </span>
             </div>
         </li>
     )
