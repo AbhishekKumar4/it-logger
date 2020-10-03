13e2ea4adfeb00a6e0ba9319f40abe93cfeeff69
Abhishek <kumarabhishek0388@gmail.com>
2020-10-03 15:25:07 +0530

    adding techItem and ad techList

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import AddBtn from './components/layout/AddBtn'
 import AddLogModal from './components/logs/AddLogModal'
 import EditLogModal from './components/logs/EditLogModal'
 import AddTechModal from './components/techs/AddTechModal'
+import TechListModal from './components/techs/TechListModal'
 
 const  App = () => {
   
@@ -24,6 +25,7 @@ const  App = () => {
         <AddLogModal />
         <EditLogModal />
         <AddTechModal />
+        <TechListModal />
         <Logs />
      </div>
     </Fragment>
