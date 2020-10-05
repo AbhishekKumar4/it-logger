6b8396e80b964a0028e26e964d647ed9b2e89d17
Abhishek <kumarabhishek0388@gmail.com>
2020-10-05 12:35:31 +0530

    connecting component to redux

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,27 +1,18 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
 import LogItem from './LogItem'
 import Preloader from '../layout/Preloader'
+import PropTypes from 'prop-types' 
+import { getLogs } from '../../actions/logActions'
 
-const Logs = () => {
 
-    const [logs, setLogs] = useState([]);
-    const [loading, setLoading] = useState(false);
-
+const Logs = ({log : { logs, loading }, getLogs}) => {
 
     useEffect(()=> {
         getLogs();
     }, []);
 
-    const getLogs = async () => {
-        setLoading(true);
-        const res = await fetch('http://localhost:5000/logs');
-        const data = await res.json();
-
-        setLogs(data);
-        setLoading(false);
-    }
-
-    if(loading) {
+    if(loading || logs === null) {
         return <Preloader />
     }
 
@@ -37,4 +28,12 @@ const Logs = () => {
     )
 }
 
-export default Logs;
+Logs.propTypes = {
+    log : PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+    log : state.log
+})
+
+export default connect(mapStateToProps, { getLogs })(Logs);
