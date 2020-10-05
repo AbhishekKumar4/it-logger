ffbb2aad6d875a4b0f3672eb66286dee0539585f
Abhishek <kumarabhishek0388@gmail.com>
2020-10-05 11:28:03 +0530

    redux initial setup

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import AddLogModal from './components/logs/AddLogModal'
 import EditLogModal from './components/logs/EditLogModal'
 import AddTechModal from './components/techs/AddTechModal'
 import TechListModal from './components/techs/TechListModal'
+import { Provider } from 'react-redux'
+import store from './store'
 
 const  App = () => {
   
@@ -18,6 +20,7 @@ const  App = () => {
     M.AutoInit();
   });
   return (
+    <Provider store={store}>
     <Fragment>
      <SearchBar />
      <div className = "container">
@@ -29,6 +32,7 @@ const  App = () => {
         <Logs />
      </div>
     </Fragment>
+    </Provider>
   );
 }
 
--- src/store.js
+++ src/store.js
@@ -1,11 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
-import { composewithDevTools } from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
 const initialState = {};
 const middleware = [thunk];
 
-const store = createStore(rootReducer, initialState, composewithDevTools(applyMiddleware(...middleware)));
+const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
 
 export default store;
